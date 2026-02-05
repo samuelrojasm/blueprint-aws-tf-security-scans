21b1f171371af5435615d097f10b5b5e91f76cf4
srojas <fesaroma@outlook.com>
2026-02-05 09:23:26 -0600

    "init: Initial commit"

--- terraform/modules/storage/s3-vulnerable/variables.tf
+++ terraform/modules/storage/s3-vulnerable/variables.tf
@@ -4,3 +4,8 @@ variable "bucket_name" {
   description = "Nombre del Bucket"
   type        = string
 }
+
+variable "tags" {
+  description = "Etiquetas comunes para todos los recursos (Tags de seguimiento)."
+  type        = map(string)
+}
