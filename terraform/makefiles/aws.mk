9d96626345df639491f80c1f37cc5aba77a2cd14
srojas <fesaroma@outlook.com>
2026-02-05 09:51:55 -0600

    init: Initial commit

---
+++ terraform/makefiles/aws.mk
@@ -0,0 +1,25 @@
+# terraform/makefiles/aws.mk
+
+# ----------------------------
+# AWS - Tareas (Targets)
+# ----------------------------
+
+# Comandos de AWS
+.PHONY: login-aws check-auth list-buckets
+
+# Autenticación
+login-aws: ## Inicia sesión en AWS SSO usando el perfil configurado
+	@echo "Iniciando sesión SSO para el perfil $(AWS_PROFILE)..."
+	@echo "Copia el código que aparecerá a continuación en tu navegador..."
+	$(DOCKER_AWS_CLI) sso login --profile $(AWS_PROFILE) --no-browser
+
+list-buckets:
+	$(DOCKER_AWS_CLI) s3 ls --profile $(AWS_PROFILE)
+
+# Target de validación
+check-auth: ## Verifica el estado de la sesión de AWS
+	@echo "Verificando sesión de AWS..."
+	@$(DOCKER_AWS_CLI) sts get-caller-identity --profile $(AWS_PROFILE) > /dev/null || \
+	(echo "Error: La sesión de AWS ha expirado. Ejecuta 'make login'"; exit 1)
+
+# ---
---
+++ terraform/makefiles/tf-s3.mk
@@ -0,0 +1,8 @@
+# terraform/makefiles/tf-s3.mk
+
+.PHONY: tf-s3-init
+
+# Inicialización con caché (Garantiza que el volumen existe)
+tf-s3-init: setup-cache check-auth ## Inicializa Terraform y descarga proveedores en caché
+	@echo "🚀 Inicializando AWS S3 en $(S3_VULNERABLE_PATH)..."
+	@$(DOCKER_TF) -chdir=$(S3_VULNERABLE_PATH) init
